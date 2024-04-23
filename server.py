df2137dd4526b8123ced61914115c1aebf3f3243
CarOcana <carocamar@outlook.es>
2024-04-23 16:21:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(mqttc, userdata, msg):
