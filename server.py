753dc13cf67dadd2ba232f61e0f1866f73439223
CarOcana <carocamar@outlook.es>
2024-04-23 15:50:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,22 +5,7 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 
-
-def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(mqttc, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-
+mqttc = mqtt.Client()
 
 mqttc.connect("mqtt://10.100.0.105", 1883)
 
