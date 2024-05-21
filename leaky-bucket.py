45762c43574c0e431b1617fe23557f444ecd6f51
CarOcana <carocamar@outlook.es>
2024-05-21 15:58:12 +0200

    Update leaky-bucket.py

--- leaky-bucket.py
+++ leaky-bucket.py
@@ -15,6 +15,9 @@ tokens = bucket_capacity
 last_check = time.time()
 lock = Lock()
 
+# Lista negra de IPs
+blacklist = set()
+
 # Verifica si hay suficientes tokens en el cubo
 def allow_request():
      global tokens, last_check
@@ -30,6 +33,9 @@ def allow_request():
      else:
          return False
 
+def is_blacklisted(ip):
+    return ip in blacklist
+
 # Rutas para gestionar las peticiones
 @app.route('/ping')
 def ping():
@@ -37,29 +43,47 @@ def ping():
 
 @app.route('/', methods=['GET'])
 def handle_get():
-     with lock:
+    client_ip = request.remote_addr
+    if is_blacklisted(client_ip):
+        return jsonify({"error": "Forbidden"}), 403
+
+    with lock:
          if not allow_request():
              return jsonify({"error": "Too many requests"}), 429
 
      # Obtener los parámetros de la URL
-     params = request.args
+    params = request.args
 
-     # Reenviar la solicitud GET al puerto 4002
-     response = requests.get('http://localhost:4002/', params=params)
-     return response.content, response.status_code
+    #Reenviar la solicitud GET al puerto 4002
+    response = requests.get('http://localhost:4002/', params=params)
+    return response.content, response.status_code
 
 @app.route('/', methods=['POST'])
 def handle_post():
-     with lock:
+    client_ip = request.remote_addr
+    if is_blacklisted(client_ip):
+        return jsonify({"error": "Forbidden"}), 403
+
+    with lock:
          if not allow_request():
              return jsonify({"error": "Too many requests"}), 429
 
-     # Obtener los datos JSON del cuerpo de la solicitud
-     data = request.json
+    #Obtener los datos JSON del cuerpo de la solicitud
+    data = request.json
 
-     # Reenviar la solicitud POST al puerto 4002
-     response = requests.post('http://localhost:4002/', json=data)
-     return response.content, response.status_code
+    # Reenviar la solicitud POST al puerto 4002
+    response = requests.post('http://localhost:4002/', json=data)
+    return response.content, response.status_code
+
+@app.route('/blacklist', methods=['POST'])
+def add_to_blacklist():
+    data = request.json
+    ip = data.get('ip')
+    if ip:
+        blacklist.add(ip)
+        return jsonify({"message": f"IP {ip} added to blacklist"}), 200
+    else:
+        return jsonify({"error": "No IP provided"}), 400
 
 # Ejecutar la aplicación Flask
 if __name__ == '__main__':
